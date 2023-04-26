4242b3bb4022e68643f95158224dcd851801dd52
Klusio19 <kusal.jakub@wp.pl>
2023-04-26 15:48:23 +0200

    Added seeder.

--- SystemSprawozdan/SystemSprawozdan.Backend/Controllers/StudentReportController.cs
+++ SystemSprawozdan/SystemSprawozdan.Backend/Controllers/StudentReportController.cs
@@ -12,28 +12,7 @@ namespace SystemSprawozdan.Backend.Controllers
     public class StudentReportController : ControllerBase
     {
         public readonly IStudentReportService _studentReportService;
-
-        public StudentReportController(IStudentReportService studentReportService)
-        {
-            _studentReportService = studentReportService;
-        }
-
-
-        [HttpPost]
-        public ActionResult SendStudentReport([FromBody] SendStudentReportDto sendStudentReportDto)
-        {
-            _studentReportService.SendStudentReport(sendStudentReportDto);
-            return Ok();
-        }
         //TODO: Jakub: Trzeba stworzyć POSTa, który umożliwi dodanie sprawozdania do odpowiedniego tematu sprawozdania, przez zalogowanego użytkownika (umieszczenie pliku oraz komentarza)
-
-
-        [HttpPut("{id}")]
-        public ActionResult EditStudentReport([FromRoute] int studentReportId)
-        {
-            _studentReportService.EditStudentReport(studentReportId);
-            return Ok();
-        }
 
         //TODO: Jakub: Trzeba stworzyć PUTa, który umożliwi edycję oddanego sprawozdania, czyli możliwość dodania kolejnego pliku
     }
