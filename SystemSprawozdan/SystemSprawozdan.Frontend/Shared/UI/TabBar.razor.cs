23f77d2c88b3aa157f7cc2a12000bab24bf1b7c4
Burit00 <burciak01@gmail.com>
2023-05-01 23:59:49 +0200

    SwitchBar refactor

--- SystemSprawozdan/SystemSprawozdan.Frontend/Shared/UI/TabBar.razor.cs
+++ SystemSprawozdan/SystemSprawozdan.Frontend/Shared/UI/TabBar.razor.cs
@@ -1,9 +1,8 @@
 ﻿namespace SystemSprawozdan.Frontend.Shared.UI
 {
-	public class SwitchBarData<T>
+	public class TabBarData<T>
 	{
 		public string Text { get; set; }
 		public T Value { get; set; }
-		public bool IsActive { get; set; } = false;
 	}
 }
