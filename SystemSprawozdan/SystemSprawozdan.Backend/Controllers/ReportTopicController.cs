94b5eef2a8c6db09aa26d74a070b6c1a21391792
QuSZo <mikolajkuszowski@gmail.com>
2023-05-09 00:18:41 +0200

    todo information

--- SystemSprawozdan/SystemSprawozdan.Backend/Controllers/ReportTopicController.cs
+++ SystemSprawozdan/SystemSprawozdan.Backend/Controllers/ReportTopicController.cs
@@ -7,6 +7,7 @@ namespace SystemSprawozdan.Backend.Controllers
     [ApiController]
     public class ReportTopicController : ControllerBase
     {
+        //TODO: KUSZO: Trzeba stworzyc GETa, ktory wyswietla wszystkie tematy sprawozdan, ktore sa przypisane do danego prowadzacego, ktory jest zalogowany
 
         //TODO: Olek: Trzeba stworzyć GETa, ktory wyswietla wszystkie tematy sprawozdan, które są przypisane do SubjectGroup, do której należy zalogowany użytkownik
 
--- SystemSprawozdan/SystemSprawozdan.Backend/Controllers/StudentReportController.cs
+++ SystemSprawozdan/SystemSprawozdan.Backend/Controllers/StudentReportController.cs
@@ -29,5 +29,6 @@ namespace SystemSprawozdan.Backend.Controllers
             return Ok();
         }
 
+
     }
 }
