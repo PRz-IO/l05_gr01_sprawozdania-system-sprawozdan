2c5fddef73c2cce0a9af0d0312bfa15b1c3b84d3
Burit00 <burciak01@gmail.com>
2023-05-19 19:33:34 +0200

    some code by pastafarian

---
+++ SystemSprawozdan/SystemSprawozdan.Frontend/Services/TeacherServices/ReportHttpService.cs
@@ -0,0 +1,26 @@
+﻿using SystemSprawozdan.Frontend.CustomClasses;
+using SystemSprawozdan.Shared.Dto;
+
+namespace SystemSprawozdan.Frontend.Services.TeacherServices;
+
+public interface IReportHttpService
+{
+    Task<List<ReportTopicGetDto>?> GetReportTopics(bool? toCheck);
+}
+
+public class ReportHttpService : IReportHttpService
+{
+    private readonly IAppHttpClient _httpClient;
+
+    public ReportHttpService(IAppHttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<ReportTopicGetDto>?> GetReportTopics(bool? toCheck)
+    {
+        var toCheckParam = new HttpParameter("toCheck", toCheck);
+        return await _httpClient.Get<List<ReportTopicGetDto>>("/StudentReport", toCheckParam);
+    }
+
+}
