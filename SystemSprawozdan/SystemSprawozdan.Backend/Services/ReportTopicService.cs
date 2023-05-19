c6b0adef91964e55acdfb4e4f6ed97e0a9ddbbfb
Burit00 <burciak01@gmail.com>
2023-05-20 00:54:03 +0200

    Backend: Moved Quszo endpoints to right controller, removed some Burit00' trash code, refactor

--- SystemSprawozdan/SystemSprawozdan.Backend/Services/ReportTopicService.cs
+++ SystemSprawozdan/SystemSprawozdan.Backend/Services/ReportTopicService.cs
@@ -1,17 +1,90 @@
-﻿namespace SystemSprawozdan.Backend.Services
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using SystemSprawozdan.Backend.Authorization;
+using SystemSprawozdan.Backend.Data;
+using SystemSprawozdan.Backend.Exceptions;
+using SystemSprawozdan.Shared.Dto;
+
+namespace SystemSprawozdan.Backend.Services
 {
     public interface IReportTopicService
     {
+        IEnumerable<ReportTopicGetDto> GetReports(bool? toCheck);
 
+        ReportTopicGetDto GetReportById(int reportTopicId);
     }
 
     public class ReportTopicService : IReportTopicService
     {
-        public ReportTopicService()
+        private readonly ApiDbContext _dbContext;
+        private readonly IUserContextService _userContextService;
+        private readonly IMapper _mapper;
+        private readonly IAuthorizationService _authorizationService;
+        public ReportTopicService(ApiDbContext dbContext, IUserContextService userContextService, IMapper mapper, IAuthorizationService authorizationService)
         {
+            _dbContext = dbContext;
+            _userContextService = userContextService;
+            _mapper = mapper;
+            _authorizationService = authorizationService;
+        }
+        
+        public IEnumerable<ReportTopicGetDto> GetReports(bool? toCheck)
+        {
+            var authorizationResult = _authorizationService.AuthorizeAsync(
+                _userContextService.User,
+                null,
+                new TeacherResourceOperationRequirement(TeacherResourceOperation.Read)).Result;
+
+            if (!authorizationResult.Succeeded)
+                throw new ForbidException();
+
+            var teacherId = _userContextService.GetUserId;
+
+            var reportsFromDb = _dbContext.ReportTopic
+                .Include(reportTopic => reportTopic.SubjectGroup)
+                .ThenInclude(subjectGroup => subjectGroup.Subject)
+                .ThenInclude(subject => subject.Major)  
+                .Where(reportTopic => reportTopic.SubjectGroup.TeacherId == teacherId);
+
+            if (toCheck != null)
+            {
+                reportsFromDb = reportsFromDb.Where(reportTopic =>
+                    reportTopic.StudentReports.Any(studentReport => studentReport.ToCheck == toCheck));
+            }
             
+            var reportsFromDbList = reportsFromDb.OrderBy(reportTopic => reportTopic.Deadline).ToList();
+            
+            var reportsDto = _mapper.Map<List<ReportTopicGetDto>>(reportsFromDbList);
+            
+            return reportsDto;
         }
 
-        //metodki
+        public ReportTopicGetDto GetReportById(int reportTopicId)
+        {
+            var authorizationResult = _authorizationService.AuthorizeAsync(
+                _userContextService.User,
+                null,
+                new TeacherResourceOperationRequirement(TeacherResourceOperation.Read)).Result;
+
+            if (!authorizationResult.Succeeded)
+                throw new ForbidException();
+
+            var teacherId = _userContextService.GetUserId;
+
+            var reportFromDb = _dbContext.ReportTopic
+                .Include(reportTopic => reportTopic.SubjectGroup)
+                .ThenInclude(subjectGroup => subjectGroup.Subject)
+                .ThenInclude(subject => subject.Major)  
+                .FirstOrDefault(reportTopic => reportTopic.SubjectGroup.TeacherId == teacherId && reportTopic.Id == reportTopicId);
+
+            if (reportFromDb is null)
+                throw new NotFoundException($"Not found report topic with Id equals {reportTopicId}!");
+            
+            var reportDto = _mapper.Map<ReportTopicGetDto>(reportFromDb);
+            
+            return reportDto;
+        }
+
     }
 }
