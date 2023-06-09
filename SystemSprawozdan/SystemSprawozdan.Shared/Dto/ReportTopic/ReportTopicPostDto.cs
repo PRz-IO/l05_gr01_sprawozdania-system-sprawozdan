4b71e65cc646d979763ab100df0996cd50ce21d4
Grzesiek <grzegorzlatawiec2@gmail.com>
2023-06-09 16:03:48 +0200

    Added report topics tab to students view and you can now add new report topic to a group.

---
+++ SystemSprawozdan/SystemSprawozdan.Shared/Dto/ReportTopic/ReportTopicPostDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SystemSprawozdan.Shared.Dto
+{
+    public class ReportTopicPostDto
+    {
+        public string Name { get; set; }  
+        public DateTime Deadline { get; set; }
+        public int GroupId { get; set; }
+    }
+}
